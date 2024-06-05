eb2353b43d88ae4c35d21006ba294b0c2060e94a
WDGonz <mrhallerx@gmail.com>
2024-06-05 20:02:54 -0300

    Create Solicitudcompra.js

---
+++ js/Solicitudcompra.js
@@ -0,0 +1,56 @@
+// Obtener el botón de comprar por su id
+const btnComprar = document.getElementById('btn-comprar');
+
+// Agregar un event listener para el evento de clic
+btnComprar.addEventListener('click', function() {
+    // Mostrar un mensaje de alerta cuando se haga clic en el botón
+    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+});
+const btnComprar2 = document.getElementById('btn-comprar2');
+
+btnComprar2.addEventListener('click', function() {
+
+    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+});
+const btnComprar3 = document.getElementById('btn-comprar3');
+
+btnComprar3.addEventListener('click', function() {
+
+    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+});
+const btnComprar4 = document.getElementById('btn-comprar4');
+
+btnComprar4.addEventListener('click', function() {
+
+    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+});
+const btnComprar5 = document.getElementById('btn-comprar5');
+
+btnComprar5.addEventListener('click', function() {
+
+    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+});
+const btnComprar6 = document.getElementById('btn-comprar6');
+
+btnComprar6.addEventListener('click', function() {
+
+    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+});
+const btnComprar7 = document.getElementById('btn-comprar7');
+
+btnComprar7.addEventListener('click', function() {
+
+    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+});
+const btnComprar8 = document.getElementById('btn-comprar8');
+
+btnComprar8.addEventListener('click', function() {
+
+    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+});
+const btnComprar9 = document.getElementById('btn-comprar9');
+
+btnComprar9.addEventListener('click', function() {
+
+    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+});
