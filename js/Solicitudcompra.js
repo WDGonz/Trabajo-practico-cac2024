99b6dd038e7f72c64c89830536e5eee2e23a5647
WDGonz <mrhallerx@gmail.com>
2024-07-09 13:31:27 -0300

    Update Solicitudcompra.js
    Se modifica el codigo js vinculado a los botones de compra de las opciones de la seccion productos por un formulario de interconexion front/back

--- js/Solicitudcompra.js
+++ js/Solicitudcompra.js
@@ -1,56 +1,63 @@
-// Obtener el botón de comprar por su id
-const btnComprar = document.getElementById('btn-comprar');
+// Mostrar el formulario cuando se hace clic en el botón de compra
+const btnComprar = document.getElementById("btn-comprar");
+const formularioCompra = document.getElementById("formulario-compra");
 
-// Agregar un event listener para el evento de clic
-btnComprar.addEventListener('click', function() {
-    // Mostrar un mensaje de alerta cuando se haga clic en el botón
-    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+btnComprar.addEventListener("click", () => {
+    formularioCompra.style.display = "block";
 });
-const btnComprar2 = document.getElementById('btn-comprar2');
 
-btnComprar2.addEventListener('click', function() {
+const btnComprar2 = document.getElementById("btn-comprar2");
+const formularioCompra2 = document.getElementById("formulario-compra2");
 
-    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+btnComprar2.addEventListener("click", () => {
+formularioCompra2.style.display = "block";
 });
-const btnComprar3 = document.getElementById('btn-comprar3');
 
-btnComprar3.addEventListener('click', function() {
+const btnComprar3 = document.getElementById("btn-comprar3");
+const formularioCompra3 = document.getElementById("formulario-compra3");
 
-    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+btnComprar3.addEventListener("click", () => {
+formularioCompra3.style.display = "block";
 });
-const btnComprar4 = document.getElementById('btn-comprar4');
 
-btnComprar4.addEventListener('click', function() {
+const btnComprar4 = document.getElementById("btn-comprar4");
+const formularioCompra4 = document.getElementById("formulario-compra4");
 
-    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+btnComprar4.addEventListener("click", () => {
+formularioCompra4.style.display = "block";
 });
-const btnComprar5 = document.getElementById('btn-comprar5');
 
-btnComprar5.addEventListener('click', function() {
+const btnComprar5 = document.getElementById("btn-comprar5");
+const formularioCompra5 = document.getElementById("formulario-compra5");
 
-    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+btnComprar5.addEventListener("click", () => {
+formularioCompra5.style.display = "block";
 });
-const btnComprar6 = document.getElementById('btn-comprar6');
 
-btnComprar6.addEventListener('click', function() {
+const btnComprar6 = document.getElementById("btn-comprar6");
+const formularioCompra6 = document.getElementById("formulario-compra6");
 
-    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+btnComprar6.addEventListener("click", () => {
+formularioCompra6.style.display = "block";
 });
-const btnComprar7 = document.getElementById('btn-comprar7');
 
-btnComprar7.addEventListener('click', function() {
+const btnComprar7 = document.getElementById("btn-comprar7");
+const formularioCompra7 = document.getElementById("formulario-compra7");
 
-    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+btnComprar7.addEventListener("click", () => {
+formularioCompra7.style.display = "block";
 });
-const btnComprar8 = document.getElementById('btn-comprar8');
 
-btnComprar8.addEventListener('click', function() {
+const btnComprar8 = document.getElementById("btn-comprar8");
+const formularioCompra8 = document.getElementById("formulario-compra8");
 
-    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+btnComprar8.addEventListener("click", () => {
+formularioCompra8.style.display = "block";
 });
-const btnComprar9 = document.getElementById('btn-comprar9');
 
-btnComprar9.addEventListener('click', function() {
+const btnComprar9 = document.getElementById("btn-comprar9");
+const formularioCompra9 = document.getElementById("formulario-compra9");
 
-    alert("Gracias por su solicitud el costo del producto es :$, estaremos contactándonos con usted para finalizar la compra solicitada.");
+btnComprar9.addEventListener("click", () => {
+formularioCompra9.style.display = "block";
 });
